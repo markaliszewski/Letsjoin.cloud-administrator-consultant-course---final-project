0a271c40b9441a79153dda8c742f04e33bef1052
Marcin Kaliszewski <hrabiadetrittus@gmail.com>
2022-11-28 00:50:45 +0100

    LJC-174 - kolejne próby transferu Email Template i Enhanced Letterhead

--- retrieved_records/deploy_records_related_to_the_LJC-174_task.bat
+++
@@ -1,9 +0,0 @@
-:: call sfdx force:data:tree:import -f EmailTemplate.json
-:: call sfdx force:data:tree:import -f EnhancedLetterhead.json
-
-call sfdx force:data:bulk:upsert -s Folder -f Folder-174.csv -i Id 
-call sfdx force:data:bulk:upsert -s EnhancedLetterhead -f Letterhead-174.csv -i Id  
-call sfdx force:data:bulk:upsert -s EmailTemplate -f Email_LJC-174.csv -i Id 
-
-
-call pause
